93c8cc24019e10cf04e0986e35278036aadbd4af
John Fitzpatrick <jfitz@computer.org>
2019-04-15 16:49:03 -0700

    FOR modifier sets and locks control variable.

--- bin/modifiers.rb
+++ bin/modifiers.rb
@@ -44,11 +44,13 @@ class IfModifier
 
   def parse_expression(expression_tokens)
     expression = nil
+
     begin
       expression = ValueScalarExpression.new(expression_tokens)
     rescue BASICExpressionError => e
       @errors << e.message
     end
+
     expression
   end
 end
@@ -63,6 +65,7 @@ class ForModifier
 
     raise(BASICSyntaxError, 'Incorrect initialization') if
       parts.size != 3
+
     raise(BASICSyntaxError, 'Missing "=" sign') if
       parts[1].to_s != '='
 
@@ -77,6 +80,7 @@ class ForModifier
     @control = VariableName.new(control_tokens[0])
     @start = ValueScalarExpression.new(start_tokens)
     @end = ValueScalarExpression.new(end_tokens)
+
     if step_tokens.nil?
       @step = nil
     else
@@ -99,6 +103,7 @@ class ForModifier
     interpreter.set_value(@control, @current_value)
     end_values = @end.evaluate(interpreter)
     end_value = end_values[0]
+
     if @step.nil?
       step_value = NumericConstant.new(1)
     else
@@ -106,12 +111,19 @@ class ForModifier
       step_value = step_values[0]
     end
 
+    interpreter.lock_variable(@control)
+    interpreter.enter_fornext(@control)
     terminated = terminated?(@current_value, step_value, end_value)
 
     io = interpreter.trace_out
     print_trace_info(io, terminated)
 
     return unless terminated
+
+    # front-terminated; go to next statement or modifier
+    interpreter.unlock_variable(@control)
+    interpreter.exit_fornext
+    @current_value = nil
 
     current_line_index = interpreter.current_line_index
     number = current_line_index.number
@@ -125,12 +137,15 @@ class ForModifier
   def execute_post(interpreter)
     end_values = @end.evaluate(interpreter)
     end_value = end_values[0]
+
     if @step.nil?
       step_value = NumericConstant.new(1)
     else
       step_values = @step.evaluate(interpreter)
       step_value = step_values[0]
     end
+
+    @current_value = interpreter.get_value(@control)
     @current_value += step_value
     interpreter.set_value(@control, @current_value)
 
@@ -141,6 +156,8 @@ class ForModifier
 
     if terminated
       @current_value = nil
+      interpreter.unlock_variable(@control)
+      interpreter.exit_fornext
     else
       current_line_index = interpreter.current_line_index
       number = current_line_index.number
@@ -157,6 +174,7 @@ class ForModifier
   def split_on_token(tokens, token_to_split)
     results = []
     list = []
+
     tokens.each do |token|
       if token.to_s != token_to_split
         list << token
@@ -166,6 +184,7 @@ class ForModifier
         results << token
       end
     end
+
     results << list unless list.empty?
     results
   end
