fde32c1bd03cd9028819b7d6895f08af3869ecd1
John Fitzpatrick <jfitz@computer.org>
2021-04-24 15:11:57 -0700

    Add the RND1() and RND2() functions.

--- bin/statements.rb
+++ bin/statements.rb
@@ -4632,11 +4632,13 @@ class ArrLetStatement < AbstractLetStatement
     l_dims = interpreter.get_dimensions(l_value.name)
     
     interpreter.set_default_args('CON1', l_dims)
+    interpreter.set_default_args('RND1', l_dims)
     interpreter.set_default_args('ZER1', l_dims)
 
     r_value = first_value(interpreter)
 
     interpreter.set_default_args('CON1', nil)
+    interpreter.set_default_args('RND1', nil)
     interpreter.set_default_args('ZER1', nil)
 
     r_dims = r_value.dimensions
@@ -5229,6 +5231,7 @@ class MatLetStatement < AbstractLetStatement
     interpreter.set_default_args('CON2', l_dims)
     interpreter.set_default_args('CON', l_dims)
     interpreter.set_default_args('IDN', l_dims)
+    interpreter.set_default_args('RND2', l_dims)
     interpreter.set_default_args('ZER2', l_dims)
     interpreter.set_default_args('ZER', l_dims)
 
@@ -5242,6 +5245,7 @@ class MatLetStatement < AbstractLetStatement
     interpreter.set_default_args('CON2', nil)
     interpreter.set_default_args('CON', nil)
     interpreter.set_default_args('IDN', nil)
+    interpreter.set_default_args('RND2', nil)
     interpreter.set_default_args('ZER2', nil)
     interpreter.set_default_args('ZER', nil)
 
