9cbec80e35c9c9433516828bbc28a2ff2bd64f0b
John Fitzpatrick <jfitz@computer.org>
2021-11-16 16:52:58 -0800

    Simplify initialization of destinations.

--- bin/program.rb
+++ bin/program.rb
@@ -341,8 +341,8 @@ class Line
     Line.new(text, @statements, tokens.flatten, @comment)
   end
 
-  def transfers_line(line_number, user_function_start_lines)
-    dests = []
+  def build_destinations(line_number)
+    @destinations = []
 
     @statements.each do |statement|
       # built-in transfers
@@ -355,15 +355,13 @@ class Line
       xfers.each do |xfer|
         # only transfers that have a different line number
         # we don't care about intra-line transfers
-        dests << TransferRefLine.new(xfer.line_number, xfer.type) if
+        @destinations << TransferRefLine.new(xfer.line_number, xfer.type) if
           xfer.line_number != line_number
       end
     end
-
-    dests
   end
 
-  def line_stmts(line_number, user_function_start_lines)
+  def line_stmts(line_number)
     dests = {}
 
     @statements.each_with_index do |statement, stmt|
@@ -1085,19 +1083,8 @@ class Program
 
   def build_line_destinations
     # build list of "gotos"
-    @lines.each { |_, line| line.destinations = [] }
-
-    destinations = {}
-
-    # for each line
-    @lines.keys.each do |line_number|
-      line = @lines[line_number]
-
-      # get destinations
-      destinations =
-        line.transfers_line(line_number, @user_function_start_lines)
-
-      line.destinations = destinations
+    @lines.each do |line_number, line|
+      line.build_destinations(line_number)
     end
   end
 
@@ -1129,11 +1116,8 @@ class Program
     # build list of "gotos"
     destinations = {}
 
-    @lines.keys.each do |line_number|
-      line = @lines[line_number]
-
-      line_destinations =
-        line.line_stmts(line_number, @user_function_start_lines)
+    @lines.each do |line_number, line|
+      line_destinations = line.line_stmts(line_number)
 
       line_destinations.each do |line_number_stmt, dests|
         destinations[line_number_stmt] = dests
