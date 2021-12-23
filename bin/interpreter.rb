278bb961ae010a11f6c88ac998dcfb8d79810ad1
John Fitzpatrick <jfitz@computer.org>
2021-12-22 20:11:08 -0800

    Add tests for RENUMBER.

--- bin/interpreter.rb
+++ bin/interpreter.rb
@@ -530,14 +530,14 @@ class Interpreter
     when 'GOTO'
       statement = GotoStatement.new(nil, [keyword], [args])
       if statement.errors.empty?
-        statement.execute(self)
+        statement.execute_core(self)
       else
         statement.errors.each { |error| @console_io.print_line(error) }
       end
     when 'LET'
       statement = LetStatement.new(nil, [keyword], [args])
       if statement.errors.empty?
-        statement.execute(self)
+        statement.execute_core(self)
       else
         statement.errors.each { |error| @console_io.print_line(error) }
       end
