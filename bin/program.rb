e1db8731dddeb0f46437b737ff4edb3a16c893c3
John Fitzpatrick <jfitz@computer.org>
2022-03-05 22:58:23 -0800

    Consistency in functions that report info on statements.

--- bin/program.rb
+++ bin/program.rb
@@ -1666,7 +1666,12 @@ class Program
         if statement.executable == :def_fn
           line_number_stmt = LineStmt.new(line_number, stmt)
           # add trace output
-          statement.print_trace_info(interpreter.trace_out, line_number_stmt)
+          interpreter.trace_out.newline_when_needed
+
+          lines = statement.trace_info(line_number_stmt)
+          lines.each { |line| interpreter.trace_out.print_out(line) }
+          interpreter.trace_out.newline
+
           statement.define_user_functions(interpreter)
         end
       end
@@ -1680,7 +1685,12 @@ class Program
         if statement.executable == :load_data
           line_number_stmt = LineStmt.new(line_number, stmt)
           # add trace output
-          statement.print_trace_info(interpreter.trace_out, line_number_stmt)
+          interpreter.trace_out.newline_when_needed
+
+          lines = statement.trace_info(line_number_stmt)
+          lines.each { |line| interpreter.trace_out.print_out(line) }
+          interpreter.trace_out.newline
+
           statement.load_data(interpreter)
         end
       end
@@ -1694,7 +1704,12 @@ class Program
         if statement.executable == :files
           line_number_stmt = LineStmt.new(line_number, stmt)
           # add trace output
-          statement.print_trace_info(interpreter.trace_out, line_number_stmt)
+          interpreter.trace_out.newline_when_needed
+
+          statement.trace_info(line_number_stmt)
+          lines.each { |line| interpreter.trace_out.print_out(line) }
+          interpreter.trace_out.newline
+
           statement.load_file_names(interpreter)
         end
       end
