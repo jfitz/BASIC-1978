69c01895587e7093746a9396d3fb946ad5ec116f
John Fitzpatrick <jfitz@computer.org>
2023-12-13 20:11:55 -0800

    Split statement keywords into lead and secondary (stmt) keywords.

--- bin/basic.rb
+++ bin/basic.rb
@@ -507,8 +507,14 @@ def make_interpreter_tokenbuilders(options, quotes, statement_separators,
   end
 
   statement_factory = StatementFactory.instance
-  keywords = statement_factory.keywords_definitions
-  tokenbuilders << ListTokenBuilder.new(keywords, KeywordToken)
+
+  # lead keywords let us identify the statement
+  lead_keywords = statement_factory.lead_keywords
+  tokenbuilders << ListTokenBuilder.new(lead_keywords, KeywordToken)
+
+  # statement keywords occur later in the text
+  stmt_keywords = statement_factory.stmt_keywords
+  tokenbuilders << ListTokenBuilder.new(stmt_keywords, KeywordToken)
 
   un_ops = UnaryOperator.operators
   bi_ops = BinaryOperator.operators
