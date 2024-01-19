74e93f19c378ecc495ae63c595fcd2f5f790c183
John Fitzpatrick <jfitz@computer.org>
2024-01-19 11:45:15 -0800

    ARR FOR locks BASE option, ARR NEXT releases the lock.

--- bin/interpreter.rb
+++ bin/interpreter.rb
@@ -215,6 +215,23 @@ class ArrForInControl < AbstractForControl
     @end = endv
   end
 
+  def enter(interpreter)
+    # lock BASE option
+    interpreter.lock_option('base')
+
+    # lock dimensions
+    super
+  end
+
+  def exit(interpreter)
+    super
+
+    # unlock dimensions
+
+    # unlock BASE option
+    interpreter.unlock_option('base')
+  end
+
   def bump_early?
     false
   end
@@ -269,9 +286,9 @@ class Interpreter
     @line_breakpoints = {}
     @line_cond_breakpoints = {}
     @locked_variables = []
-    @locked_arr_dims = []
-    @locked_mat_dims = []
-    @locked_options = []
+    @locked_arr_dims = {}
+    @locked_mat_dims = {}
+    @locked_options = {}
     @loop_stack = []
     @data_store = DataStore.new
     @file_handlers = {}
@@ -424,6 +441,8 @@ class Interpreter
       raise(BASICCommandError, text)
     end
 
+    @locked_options = {}
+
     @program.reset_profile_metrics
 
     @step_mode = false
@@ -431,11 +450,17 @@ class Interpreter
     @trace_out = trace ? @console_io : @null_out
 
     @variables = {}
+    @locked_variables = []
+    @locked_arr_dims = {}
+    @locked_mat_dims = {}
+
     @data_store = DataStore.new
+
     @user_function_defs = {}
 
     @previous_stack = []
     clear_previous_lines
+
     @start_time = Time.now
     run_program
   end
@@ -980,7 +1005,7 @@ class Interpreter
 
   def push_option(name, v)
     if @locked_options.include?(name)
-      raise BASICRuntimeError.new(:te_opt_lock, name)
+      raise BASICRuntimeError.new(:te_option_lock, name)
     end
 
     $options[name].push(v)
@@ -995,7 +1020,7 @@ class Interpreter
 
   def pop_option(name)
     if @locked_options.include?(name)
-      raise BASICRuntimeError.new(:te_opt_lock, name)
+      raise BASICRuntimeError.new(:te_option_lock, name)
     end
 
     v = $options[name].pop
@@ -1580,19 +1605,21 @@ class Interpreter
   end
 
   def lock_option(name)
-    if @locked_options.include?(name)
-      raise BASICRuntimeError.new(:te_option_lock, name)
-    end
+    # allow multiple locks; first one makes an entry of '1'
+    @locked_options[name] = 0 unless @locked_options.include?(name)
 
-    @locked_options << name
+    @locked_options[name] += 1
   end
 
   def unlock_option(name)
+    # there should be at least one lock
     unless @locked_options.include?(name)
       raise BASICRuntimeError.new(:te_option_no_lock, name)
     end
 
-    @locked_options.delete(name)
+    # allow multiple locks; removal of last deletes the entry
+    @locked_options[name] -= 1
+    @locked_options.delete(name) if @locked_options[name] < 1
   end
 
   def push_return(destination)
